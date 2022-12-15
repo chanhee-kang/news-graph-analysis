98fcda0c01d2661a56ec85e2056f1e48d2b9dc38
chanhee-kang <chk971001@gmail.com>
2022-12-15 16:03:36 +0900

    NetworkX레이아웃 추가

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -4,11 +4,15 @@ import json
 import re
 import pandas as pd
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
+from networkx.readwrite import json_graph
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from fa2 import ForceAtlas2
 from tqdm import tqdm
 from typing import List
 from PyKomoran import *
@@ -70,10 +74,6 @@ def term_matrix():
                                        columns = tfidf_vectorizer.vocabulary_)
     return tfidf_term_term_mat
 
-import networkx as nx
-import matplotlib.pyplot as plt
-# %matplotlib inline
-from fa2 import ForceAtlas2
 
 df = term_matrix()
 graph = nx.from_numpy_matrix(df.values)
@@ -95,31 +95,12 @@ forceatlas2 = ForceAtlas2(outboundAttractionDistribution=True,  # Dissuade hubs
 
                           # Log
                           verbose=True)
-
-G = graph
 positions = forceatlas2.forceatlas2_networkx_layout(G, pos=None, iterations=2000)
-
-# nx.draw_networkx_nodes(G, positions, node_size=20, with_labels=False, node_color="blue", alpha=0.4)
 nx.draw_networkx_nodes(G, positions, node_size=20, label=True, node_color="blue", alpha=0.4)
 nx.draw_networkx_edges(G, positions, edge_color="green", alpha=0.05)
 
+G = graph
 
-from networkx.readwrite import json_graph
-from random import sample
-
-
-# Gold standard data of positive gene functional associations
-# from https://www.inetbio.org/wormnet/downloadnetwork.php
-#G = nx.read_edgelist("WormNet.v3.benchmark.txt")
-
-# remove randomly selected nodes (to make example fast)
-num_to_remove = int(len(G) / 1.5)
-nodes = sample(list(G.nodes), num_to_remove)
-G.remove_nodes_from(nodes)
-
-# remove low-degree nodes
-low_degree = [n for n, d in G.degree() if d < 10]
-G.remove_nodes_from(low_degree)
 
 # largest connected component
 components = nx.connected_components(G)
@@ -133,11 +114,22 @@ centrality = nx.betweenness_centrality(H, k=10, endpoints=True)
 lpc = nx.community.label_propagation_communities(H)
 community_index = {n: i for i, com in enumerate(lpc) for n in com}
 
+###레이아웃별로 뭐가 이쁠지 테스트 해보기!
+layouts = {'spring': nx.spring_layout(G), 
+           'spectral':nx.spectral_layout(G), 
+           'shell':nx.shell_layout(G), 
+           'circular':nx.circular_layout(G),
+           'kamada_kawai':nx.kamada_kawai_layout(G), 
+           'random':nx.random_layout(G)
+          }
+
+
 #### draw graph ####
 fig, ax = plt.subplots(figsize=(20, 15))
 pos = nx.spring_layout(H, k=0.15, seed=4572321)
 node_color = [community_index[n] for n in H]
 node_size = [v * 20000 for v in centrality.values()]
+
 nx.draw_networkx(
     H,
     pos=pos,
@@ -150,9 +142,8 @@ nx.draw_networkx(
 
 # Title/legend
 font = {"color": "k", "fontweight": "bold", "fontsize": 20}
-ax.set_title("Gene functional association network (C. elegans)", font)
-# Change font color for legend
-font["color"] = "r"
+ax.set_title("테스트!", font)
+font["color"] = "r" # Change font color for legend
 
 ax.text(
     0.80,
@@ -176,3 +167,5 @@ ax.margins(0.1, 0.05)
 fig.tight_layout()
 plt.axis("off")
 plt.savefig("savefig_default.png")
+
+json_graph.node_link_data(H)
