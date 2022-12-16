9916d0cccaeee09701da77e6c58ff2ca992e4781
chanhee-kang <chk971001@gmail.com>
2022-12-16 14:50:07 +0900

    model 수정

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -8,12 +8,12 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import pickle
 
-from networkx.readwrite import json_graph
+# from networkx.readwrite import json_graph
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from fa2 import ForceAtlas2
+# from fa2 import ForceAtlas2
 from tqdm import tqdm
 from typing import List
 from PyKomoran import *
@@ -36,9 +36,9 @@ def load_stopwords(path : str) -> List[str]:
     ff.close()
     return data.split("\n")
 
-def load_stopwords(path : str) -> List[str]:
-    with open(path, "r") as f :
-        return f.split("\n")
+# def load_stopwords(path : str) -> List[str]:
+#     with open(path, "r") as f :
+#         return f.split("\n")
 
 def tokenize(filepath : str, openwith : str) -> pd.DataFrame :    
     if openwith == "url" :
@@ -81,102 +81,102 @@ def term_matrix(filepath, openwith) -> pd.DataFrame :
                                        columns = tfidf_vectorizer.vocabulary_)
     return tfidf_term_term_mat
 
-if __name__ == "__main__":
-    filepath = "https://raw.githubusercontent.com/e9t/nsmc/master/synopses.json"
-    dummy = term_matrix(filepath, openwith = "url")
-    with open('../datasets/matrix/term_matrix_test.pickle', 'wb') as f:
-        pickle.dump(dummy, f, pickle.HIGHEST_PROTOCOL)
+# if __name__ == "__main__":
+#     filepath = "https://raw.githubusercontent.com/e9t/nsmc/master/synopses.json"
+#     dummy = term_matrix(filepath, openwith = "url")
+#     with open('../datasets/matrix/term_matrix_test.pickle', 'wb') as f:
+#         pickle.dump(dummy, f, pickle.HIGHEST_PROTOCOL)
 
-df = term_matrix()
-graph = nx.from_numpy_matrix(df.values)
+# df = term_matrix()
+# graph = nx.from_numpy_matrix(df.values)
 
-forceatlas2 = ForceAtlas2(outboundAttractionDistribution=True,  # Dissuade hubs
-                          linLogMode=False,  # NOT IMPLEMENTED
-                          adjustSizes=False,  # Prevent overlap (NOT IMPLEMENTED)
-                          edgeWeightInfluence=1.0,
+# forceatlas2 = ForceAtlas2(outboundAttractionDistribution=True,  # Dissuade hubs
+#                           linLogMode=False,  # NOT IMPLEMENTED
+#                           adjustSizes=False,  # Prevent overlap (NOT IMPLEMENTED)
+#                           edgeWeightInfluence=1.0,
                           
-                          jitterTolerance=5.0,  # Tolerance
-                          barnesHutOptimize=True,
-                          barnesHutTheta=1.2,
-                          multiThreaded=False,  # NOT IMPLEMENTED
+#                           jitterTolerance=5.0,  # Tolerance
+#                           barnesHutOptimize=True,
+#                           barnesHutTheta=1.2,
+#                           multiThreaded=False,  # NOT IMPLEMENTED
 
-                          # Tuning
-                          scalingRatio=2.0,
-                          strongGravityMode=False,
-                          gravity=1.0,
+#                           # Tuning
+#                           scalingRatio=2.0,
+#                           strongGravityMode=False,
+#                           gravity=1.0,
 
-                          # Log
-                          verbose=True)
-positions = forceatlas2.forceatlas2_networkx_layout(G, pos=None, iterations=2000)
-nx.draw_networkx_nodes(G, positions, node_size=20, label=True, node_color="blue", alpha=0.4)
-nx.draw_networkx_edges(G, positions, edge_color="green", alpha=0.05)
+#                           # Log
+#                           verbose=True)
+# positions = forceatlas2.forceatlas2_networkx_layout(G, pos=None, iterations=2000)
+# nx.draw_networkx_nodes(G, positions, node_size=20, label=True, node_color="blue", alpha=0.4)
+# nx.draw_networkx_edges(G, positions, edge_color="green", alpha=0.05)
 
-G = graph
-# largest connected component
-components = nx.connected_components(G)
-largest_component = max(components, key=len)
-H = G.subgraph(largest_component)
+# G = graph
+# # largest connected component
+# components = nx.connected_components(G)
+# largest_component = max(components, key=len)
+# H = G.subgraph(largest_component)
 
-# compute centrality
-centrality = nx.betweenness_centrality(H, k=10, endpoints=True)
+# # compute centrality
+# centrality = nx.betweenness_centrality(H, k=10, endpoints=True)
 
-# compute community structure
-lpc = nx.community.label_propagation_communities(H)
-community_index = {n: i for i, com in enumerate(lpc) for n in com}
+# # compute community structure
+# lpc = nx.community.label_propagation_communities(H)
+# community_index = {n: i for i, com in enumerate(lpc) for n in com}
 
-###레이아웃별 테스트
-layouts = {'spring': nx.spring_layout(G), 
-           'spectral':nx.spectral_layout(G), 
-           'shell':nx.shell_layout(G), 
-           'circular':nx.circular_layout(G),
-           'kamada_kawai':nx.kamada_kawai_layout(G), 
-           'random':nx.random_layout(G)
-          }
-
-
-#### draw graph ####
-fig, ax = plt.subplots(figsize=(20, 15))
-pos = nx.spring_layout(H, k=0.15, seed=4572321)
-node_color = [community_index[n] for n in H]
-node_size = [v * 20000 for v in centrality.values()]
+# ###레이아웃별 테스트
+# layouts = {'spring': nx.spring_layout(G), 
+#            'spectral':nx.spectral_layout(G), 
+#            'shell':nx.shell_layout(G), 
+#            'circular':nx.circular_layout(G),
+#            'kamada_kawai':nx.kamada_kawai_layout(G), 
+#            'random':nx.random_layout(G)
+#           }
 
 
-nx.draw_networkx(
-    H,
-    pos=pos,
-    with_labels=False,
-    node_color=node_color,
-    node_size=node_size,
-    edge_color="gainsboro",
-    alpha=0.4,
-)
+# #### draw graph ####
+# fig, ax = plt.subplots(figsize=(20, 15))
+# pos = nx.spring_layout(H, k=0.15, seed=4572321)
+# node_color = [community_index[n] for n in H]
+# node_size = [v * 20000 for v in centrality.values()]
 
-# Title/legend
-font = {"color": "k", "fontweight": "bold", "fontsize": 20}
-ax.set_title("테스트!", font)
-font["color"] = "r" # Change font color for legend
 
-ax.text(
-    0.80,
-    0.10,
-    "node color = community structure",
-    horizontalalignment="center",
-    transform=ax.transAxes,
-    fontdict=font,
-)
-ax.text(
-    0.80,
-    0.06,
-    "node size = betweeness centrality",
-    horizontalalignment="center",
-    transform=ax.transAxes,
-    fontdict=font,
-)
+# nx.draw_networkx(
+#     H,
+#     pos=pos,
+#     with_labels=False,
+#     node_color=node_color,
+#     node_size=node_size,
+#     edge_color="gainsboro",
+#     alpha=0.4,
+# )
 
-# Resize figure for label readibility
-ax.margins(0.1, 0.05)
-fig.tight_layout()
-plt.axis("off")
-plt.savefig("../figure/savefig_default.png")
+# # Title/legend
+# font = {"color": "k", "fontweight": "bold", "fontsize": 20}
+# ax.set_title("테스트!", font)
+# font["color"] = "r" # Change font color for legend
 
-json_graph.node_link_data(H)
+# ax.text(
+#     0.80,
+#     0.10,
+#     "node color = community structure",
+#     horizontalalignment="center",
+#     transform=ax.transAxes,
+#     fontdict=font,
+# )
+# ax.text(
+#     0.80,
+#     0.06,
+#     "node size = betweeness centrality",
+#     horizontalalignment="center",
+#     transform=ax.transAxes,
+#     fontdict=font,
+# )
+
+# # Resize figure for label readibility
+# ax.margins(0.1, 0.05)
+# fig.tight_layout()
+# plt.axis("off")
+# plt.savefig("../figure/savefig_default.png")
+
+# json_graph.node_link_data(H)
